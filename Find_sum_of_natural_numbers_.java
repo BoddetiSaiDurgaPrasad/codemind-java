6e91dc48b78a8fcbbe944862621423473d9c00c4
BoddetiSaiDurgaPrasad <94205751+BoddetiSaiDurgaPrasad@users.noreply.github.com>
2022-11-09 11:39:16 +0530

    import java.util.*; class psa {     public static void main(String[]args)     {         Scanner sc=new Scanner(System.in);         int s=sc.nextInt();         System.out.println((s*(s+1))/2);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class psa
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        System.out.println((s*(s+1))/2);
+    }
+}
